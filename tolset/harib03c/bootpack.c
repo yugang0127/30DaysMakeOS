a58f10439beb96aa4688b2c3dd287c7f09686c91
Yu Gang <yugang@xin.com>
2019-03-18 09:19:54 +0800

    06 day practise finished

---
+++ tolset/harib03c/bootpack.c
@@ -0,0 +1,28 @@
+/* bootpack的主体*/
+
+#include "bootpack.h"
+#include <stdio.h>
+
+void HariMain(void)
+{
+	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
+	char s[40], mcursor[256];
+	int mx, my;
+
+	init_gdtidt();
+	init_palette();
+	init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);
+	mx = (binfo->scrnx - 16) / 2;
+	my = (binfo->scrny - 28 - 16) / 2;
+	init_mouse_cursor8(mcursor, COL8_008484);
+	putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
+	sprintf(s, "scrnx = %d", binfo->scrnx);
+	putfont8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
+
+	for (;;) {
+		io_hlt();
+	}
+}
+
+
+
