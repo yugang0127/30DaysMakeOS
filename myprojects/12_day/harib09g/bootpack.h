70ab82add91f67b20ad41c9082853be75203324f
Yu Gang <yugang@xin.com>
2019-04-04 08:32:17 +0800

    fix day 12 bugs

--- myprojects/12_day/harib09g/bootpack.h
+++ myprojects/12_day/harib09g/bootpack.h
@@ -174,7 +174,7 @@ struct TIMER {
 };
 struct TIMERCTL {
 	unsigned int count, next, using;
-	struct TIMER timer[MAX_TIMER];
+	struct TIMER *timers[MAX_TIMER];
 	struct TIMER timers0[MAX_TIMER];
 };
 extern struct TIMERCTL timerctl;
